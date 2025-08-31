7b35d911f59f1ba1fbfe8a47ceae4e22d5370ef3
demogit1 <sumanu9400@gmail.com>
2025-08-31 10:27:18 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+#create project structure
+mkdir -p src
+mkdir -p research
+
+#create file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully."
